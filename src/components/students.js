74fa54ef59839dc80a33646e8dbba31818098dd9
kogepanh <k.hosono1209@gmail.com>
2021-06-09 15:02:05 +0900

    update

--- src/components/students.js
+++ src/components/students.js
@@ -1,17 +1,27 @@
 import Image from "next/image";
 
-function Students({ posts }) {
+function Students({ allLogs }) {
   return (
     <section>
       <p>This is Students component.</p>
-      <p>{posts}</p>
+      <p>{ allLogs }</p>
     </section>
   )
+}
+
+Students.getInitialProps = async () => {
+  const res = await fetch(process.env.API_URL + "/logs");
+  const posts = await res.json();
+  console.log(res);
+  console.log(posts);
+
+  return {allLogs: posts};
 }
 
 export async function getStaticProps() {
   const res = await fetch(process.env.API_URL + "/logs");
   const posts = await res.json();
+  console.log(res);
   console.log(posts);
 
   return {
