d57eddd4794ae6e1749e9e846c8240ef3c240427
kogepanh <k.hosono1209@gmail.com>
2021-06-13 23:01:26 +0900

    update props

--- src/components/students.js
+++ src/components/students.js
@@ -1,43 +1,16 @@
 import React, { Component } from "react";
-
-import axios from "axios";
+import PropTypes from "prop-types";
 
 import Student from "../components/student.js";
 
-class Studnets extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      logs: {},
-    };
-  }
-
-  componentDidMount() {
-    axios.get(process.env.REACT_APP_API_URL + "/logs").then((results) => {
-      const items = results.data.Items;
-
-      const logs = {};
-      items.map((item) => {
-        if (!logs[item.id]) {
-          logs[item.id] = {};
-          logs[item.id].j_full_name = item.j_full_name;
-          logs[item.id].e_full_name = item.e_full_name;
-          logs[item.id].logs = [];
-        }
-        logs[item.id].logs.push({ data: item.date, count: item.stay_duration });
-      });
-
-      this.setState({ logs: logs });
-      // console.log(logs);
-    });
-  }
-
+class Students extends Component {
   render() {
+    const logs = this.props.logs;
     return (
       <section>
-        {Object.keys(this.state.logs).map((key) => (
+        {Object.keys(logs).map((key) => (
           <div key={key}>
-            <Student logs={this.state.logs[key]} />
+            <Student logs={logs[key]} />
           </div>
         ))}
       </section>
@@ -45,4 +18,8 @@ class Studnets extends Component {
   }
 }
 
-export default Studnets;
+Students.propTypes = {
+  logs: PropTypes.object,
+};
+
+export default Students;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,44 @@
 import React, { Component } from "react";
 
+import axios from "axios";
+
 import Header from "../components/header.js";
 import Students from "../components/students.js";
 
 class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logs: {},
+    };
+  }
+
+  componentDidMount() {
+    axios.get(process.env.REACT_APP_API_URL + "/logs").then((results) => {
+      const items = results.data.Items;
+
+      const logs = {};
+      items.map((item) => {
+        if (!logs[item.id]) {
+          logs[item.id] = {};
+          logs[item.id].j_full_name = item.j_full_name;
+          logs[item.id].e_full_name = item.e_full_name;
+          logs[item.id].logs = [];
+        }
+        logs[item.id].logs.push({ data: item.date, count: item.stay_duration / 1000 }); // ミリ秒を秒に変換
+      });
+
+      this.setState({ logs: logs });
+      // console.log(logs);
+    });
+  }
+
   render() {
     return (
       <>
         <Header />
         <main>
-          <Students />
+          <Students logs={this.state.logs} />
         </main>
       </>
     );
